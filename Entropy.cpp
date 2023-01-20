b6b1da35e7964dcbd04427319f42ed541600a69e
LakeevGeorgiy <113018334+LakeevGeorgiy@users.noreply.github.com>
2023-01-20 18:45:16 +0300

    Update Entropy.cpp

--- Entropy.cpp
+++ Entropy.cpp
@@ -12,6 +12,7 @@ int main(){
     map<char, map<char, uint32_t>> freq_last_symbols;
     uint32_t cnt_symbols = 1;
 
+    // с - текущий считываемый символ, last_symbol - предыдущий символ, freq_symbol[i] - кол-во i-символов в файле.
     char c;
     char last_symbol;
     in.get(last_symbol);
@@ -26,11 +27,13 @@ int main(){
         cnt_symbols++;
     }
 
+    // probability[i] - вероятность появление i-го символа в файле
     std::map<char, long double> probability;
     for (auto& freq_symbol : freq_symbols){
         probability[freq_symbol.first] = static_cast<long double>(freq_symbol.second) / cnt_symbols;
     }
 
+    // Подсчет границы Шеннона по формуле
     long double low_limit = 0;
     for (auto& prob : probability){
         long double bits = log2(prob.second) * prob.second * (-1);
@@ -38,6 +41,7 @@ int main(){
     }
     std::cout << "low limit: " << low_limit << "\n";
 
+    // Подсчет условной энтропии по формуле
     long double conditional_entropy = 0;
     for (auto& symbol : freq_last_symbols){
         long double sum_bits = 0;
